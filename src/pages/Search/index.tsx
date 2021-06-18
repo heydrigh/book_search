ce9b1ec8da678cb8dce1603a0203317f6775a38f
heydrigh <heydrigh@gmail.com>
2021-06-17 23:32:42 -0300

    feat: add pages structure

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+
+const Search = () => {
+  return (
+  <S.Wrapper>
+    <h1>Search</h1>
+  </S.Wrapper>
+)};
+
+export default Search;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Details from "../pages/Details";
+import Search from "../pages/Search";
 
 const Routes = () => (
-	<BrowserRouter>
-		<Switch>
-			<Route path="/" exact component={Home} />
-		</Switch>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/search" exact component={Search} />
+      <Route path="/details/:id" exact component={Details} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
